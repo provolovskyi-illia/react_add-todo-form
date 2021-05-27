4b672d9e4d388edc7a1147175d8100cf2b78db19
Provolovskyi Illia <provolovskyi.illia@gmail.com>
2021-05-27 17:34:21 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.2e44268abd4cd21634437b79e3c97826.js"
 );
 
 self.addEventListener('message', (event) => {
